f5105e341c8069c805328e58bb8b2d36329a8439
Mayank Dedhia <mayankdedhia@gmail.com>
2020-10-29 15:45:30 +0530

    Add a check for access_token being stored after the auth request.

--- spec/paychex/client/auth_spec.rb
+++ spec/paychex/client/auth_spec.rb
@@ -7,13 +7,15 @@ RSpec.describe "Paychex auth to" do
         :body => fixture("auth.json"),
         :headers => { :content_type => "application/json; charset=utf-8" },
       )
-      response = Paychex.client().authorize({
+      client = Paychex.client()
+      response = client.authorize({
         grant_type: "client_credentials",
         client_id: "c22",
         client_secret: "8f6a4213",
       })
       expect(response.status).to eq(200)
       expect(response.body["access_token"]).to eq("211fe7540e")
+      expect(client.access_token).to eq(response.body["access_token"])
       expect(response.body["expires_in"]).to be 3600
     end
   end
